17a50fbcf031fbe4ce8c460e871a3864de6b8b7b
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2017-10-10 11:35:01 +0200

    example 1

--- index.php
+++ index.php
@@ -1,14 +1,79 @@
 <?php
 
-// Autoloader implementaion
+define('BP', __DIR__ . '/');
+define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
+define('DEVELOP', true);
 
-// TODO: add code here ...
+// Reset include paths
+set_include_path(implode(PS, [
+    BP . 'lib/',
+    BP . 'src/app/',
+    BP . 'src/core/'
+]));
 
-/** @var Logger $logger */
-$logger = new \Logger();
+// Setup autoloader
+spl_autoload_register(function($class) {
+    include str_replace('\\', DS, $class) . '.php';
+});
 
-/** @var Application $application */
-$application = new \Application();
+// PHP setting for application behaviour
+if (DEVELOP) {
+    error_reporting(-1);
+    ini_set('display_errors', 1);
+    ini_set('log_errors', 0);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+    ini_set('log_errors', 1);
+    ini_set('error_log', BP . 'var/log/php-error.log');
+}
 
-/** @var \Db\Connection $dbConnection */
-$dbConnection = new \Db\Connection;
+///////////////////////////////
+
+$timeStart = microtime(true);
+
+/** @var Db\Connection $db */
+$connection = \Db\Connection::getInstance([
+    'host'  => 'localhost',
+    'user'  => 'keh',
+    'pass'  => 'keh',
+    'name'  => 'keh'
+]);
+
+/** @var PDO $db */
+$db = $connection->getDb();
+
+/** @var PDOStatement $sql */
+$sql = $db->prepare('SELECT * FROM `catalog_product_entity_varchar`');
+$sql->execute();
+
+
+
+// Method 1
+//$results = $sql->fetchAll();
+//foreach ($results as $result) {
+//    echo $result['value'] . PHP_EOL;
+//}
+
+// Method 2
+while ($result = $sql->fetch()) {
+    echo $result['value'] . PHP_EOL;
+}
+
+
+
+
+
+
+
+
+echo PHP_EOL . PHP_EOL . '-------' . PHP_EOL;
+
+$timeEnd = microtime(true);
+$total = $timeEnd - $timeStart;
+
+echo 'Executed in ' . sprintf("%01.2f", $total) . ' sec' . PHP_EOL;
+
+$mem = (memory_get_peak_usage(true) / 1024 / 1024);
+echo "Memory usage: {$mem} MB" . PHP_EOL;
--- src/core/Db/Connection.php
+++ src/core/Db/Connection.php
@@ -10,12 +10,62 @@ namespace Db;
 class Connection
 {
     /**
-     * TODO: Implement as singleton
-     *
-     * Connection constructor.
+     * @var Connection|null
      */
-    function __construct()
+    private static $instance = null;
+
+    /**
+     * @var \Pdo|null
+     */
+    private $db = null;
+
+    private $config = [];
+
+    /**
+     * Connection constructor.
+     * @param string[] $config
+     */
+    private function __construct($config)
     {
-        echo "Connection oppened";
+        $this->config = $config;
+    }
+
+    /**
+     * Returns the singleton instance
+     *
+     * @param string[] $config
+     * @return Connection
+     */
+    public static function getInstance($config): Connection
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($config);
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Returns the PDO instance
+     *
+     * @return \PDO
+     */
+    public function getDb(): \PDO
+    {
+        if ($this->db === null) {
+
+            $dsn = 'mysql:host=' . $this->config['host'] . ';dbname=' . $this->config['name'] . ';charset=utf8';
+
+            $this->db = new \PDO(
+                $dsn,
+                $this->config['user'],
+                $this->config['pass']
+            );
+
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        }
+
+        return $this->db;
     }
 }
